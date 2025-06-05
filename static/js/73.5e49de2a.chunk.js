8594c24ae874ebc5f2bbbe6518a0638b5d47cf9e
Iulian Baranai <iulian.baranai@gmail.com>
2025-06-05 18:24:22 +0200

    Initial commit

---
+++ static/js/73.5e49de2a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkij_fysio=self.webpackChunkij_fysio||[]).push([[73],{267:(s,e,n)=>{n.r(e),n.d(e,{default:()=>c});var r=n(470),a=n(201),j=n(181),i=n(948),o=n(474),t=n(579);function c(){return(0,t.jsx)(j.i,{header:(0,t.jsx)(o.Ce,{}),body:(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(a.yy,{space:25}),(0,t.jsx)(o.zi,{}),(0,t.jsx)(o.Hb,{}),(0,t.jsx)(i.A0,{children:(0,t.jsx)(a.Ld,{children:r.F.ontstaan.text,color:"zand"}),background:"oranje"})]}),footer:(0,t.jsx)(o.Gn,{})})}}}]);
+//# sourceMappingURL=73.5e49de2a.chunk.js.map
